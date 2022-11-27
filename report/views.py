33821fe2beb76584c37b650925be047acbc5fe4e
Aref Farzanieh <A.Farzanieh@digikala.com>
2022-11-27 14:56:37 +0330

    stock warehouse added

--- report/views.py
+++ report/views.py
@@ -13,6 +13,17 @@ from plotly.offline import plot
 from plotly.graph_objs import Scatter
 df=pd.read_csv('df.csv')
 df_dg=pd.read_csv('digital.csv')
+stock=pd.read_csv('stock.csv')
+def chart(feature):
+    try:
+        if feature=='month year':
+            fig = px.bar(stock.groupby(feature)['Price'].sum())
+            fig.update_layout(xaxis=dict(tickformat="%Y-%m"))
+            
+        else: fig = px.bar(stock.groupby(feature)['Price'].sum())
+        return fig
+    except KeyError as e:
+        print(f'There is no column named {e}, Please choose a right column')
 def get_sum(warehouse_id):
     s=df[df['warehouse id']==warehouse_id]
     total = s['purchase price'].sum()
@@ -43,6 +54,15 @@ class IndexView(View):
         fig3.update_layout(template='plotly_dark',title='Treemap separated by Brand')
         chart3=fig3.to_html()
 
-        return render(request,'report/index.html',{'six':six,'five':five,'chart':chart,'chart2':chart2,'chart3':chart3})
+        fig4=px.bar(stock.groupby('month year')['Price'].sum())
+        fig4.update_layout(template='plotly_dark',title='Amount per month 1401',xaxis=dict(tickformat="%Y-%m"),xaxis_title='Time')
+        chart4=fig4.to_html
+
+        
+        fig5=px.bar(stock.groupby('Cost of sales type')['Price'].sum())
+        fig5.update_layout(template='plotly_dark',title='Cost of sales type')
+        chart5=fig5.to_html
+
+        return render(request,'report/index.html',{'six':six,'five':five,'chart':chart,'chart2':chart2,'chart3':chart3,'chart4':chart4,'chart5':chart5})
     def post(self,request):
         return render(request,'report/index.html')
